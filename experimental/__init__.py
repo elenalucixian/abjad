59782b295ba6f152a8b199f784368d1cbc77e7b0
Trevor Bača <trevor.baca@gmail.com>
2017-08-04 11:34:40 -0500

    Added SegmentMaker to experimental global namespace.

--- experimental/__init__.py
+++ experimental/__init__.py
@@ -1,8 +1,6 @@
 # -*- encoding: utf-8 -*-
 from __future__ import absolute_import
-from abjad import *
-
-
 from experimental.tools import constrainttools
 from experimental.tools import interpolationtools
 from experimental.tools import makertools
+from experimental.tools.makertools import SegmentMaker
